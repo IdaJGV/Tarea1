957039994817c70c3b35fce4e33df2604748311a
IdaJGV <109998563+IdaJGV@users.noreply.github.com>
2022-07-28 14:59:21 -0600

    primer commit

---
+++ default.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    <header>
+        <h1>MI HOJA DE VIDA</h1>
+    </header>
+
+    <div class="contenedor">
+        <nav>
+            <ul>
+                <li><a href="#">Bienvenida</a></li>
+                <li><a href="datos.html">Datos Personales</a></li>
+                <li><a href="experiencia.html">Experiencia Laboral</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="imagen">
+        <p> 
+            <img src="imagen.png"    alt="Muesta imagen DW">
+        </p>
+    </div>
+    <footer>
+        Todos los derechos reservados a: &copyIdair Josué Girón Villatoro 
+    </footer>
+</body>
+</html>
